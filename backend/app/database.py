a67d88a0a7401e708365f3c723ac676d23201114
Tejashree <23eg105r57@anurag.edu.in>
2026-05-24 17:32:58 +0530

    fixed database url for deployment

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,20 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-from dotenv import load_dotenv
-import os
-from pathlib import Path
+from sqlalchemy.orm import sessionmaker
 
-env_path = Path(__file__).parent / ".env"
-load_dotenv(dotenv_path=env_path)
+DATABASE_URL = "sqlite:///./test.db"
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
-
-Base = declarative_base()
